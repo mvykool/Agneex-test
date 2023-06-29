b1a9c55652beaf94d70855857eed39df01e51490
maicol hernandez <maykolandres4@gmail.com>
2023-06-29 10:21:19 -0500

    created project and did UI

---
+++ agneex-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ agneex-app/src/components/HomeNavbar.jsx
@@ -0,0 +1,15 @@
+
+
+const HomeNavbar = () => {
+return (
+	<nav>
+		<div className="nav-bar">
+			<a href="#" className="nav-link">Gmail</a>
+			<a href="#" className="nav-link">Imágenes</a>
+			<button className="nav-btn">Acceder</button>
+		</div>
+	</nav>
+)
+}
+
+export default HomeNavbar
