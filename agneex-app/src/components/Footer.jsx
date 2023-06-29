5024e8d8bac3861b7185b40ef832f5bf0f13667f
maicol hernandez <maykolandres4@gmail.com>
2023-06-29 10:26:12 -0500

    advanced in home ui

--- agneex-app/src/components/Footer.jsx
+++ agneex-app/src/components/Footer.jsx
@@ -2,7 +2,23 @@
 
 const Footer = () => {
   return (
-	<div>Footer</div>
+	<footer>
+    <div>Colombia</div>
+    <div>
+      <ul>
+        <li>Sobre Google</li>
+        <li>Publicidad</li>
+        <li>Negocios</li>
+        <li>Cómo funciona la Búsqueda</li>
+      </ul>
+
+      <ul>
+        <li>Privacidad</li>
+        <li>Condiciones</li>
+        <li>Preferencias</li>
+      </ul>
+    </div>
+  </footer>
   )
 }
 
