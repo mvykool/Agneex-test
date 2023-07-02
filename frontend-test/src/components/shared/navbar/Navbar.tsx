09f01b2d1a514e9b4e58a9c67344160250ccf811
maicol hernandez <maykolandres4@gmail.com>
2023-07-02 10:04:23 -0500

    added tablet UI media queries

--- frontend-test/src/components/shared/navbar/Navbar.tsx
+++ frontend-test/src/components/shared/navbar/Navbar.tsx
@@ -1,68 +1,70 @@
-	import { useContext, useEffect } from 'react';
-	import { useLocation, Link } from 'react-router-dom';
-	import Logo from "../../../assets/images/logo.png";
-	import Search from '../search/Search';
-	import { SearchContext } from '../../../context/SearchContext';
-	import './Navbar.css';
+import { useContext, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import Logo from "../../../assets/images/logo.png";
+import Search from '../search/Search';
+import { SearchContext } from '../../../context/SearchContext';
+import './Navbar.css';
 
-	const Navbar = () => {
-	const { searchTerm, setSearchTerm } = useContext(SearchContext);
-	const location = useLocation();
-	const isResultsPage = location.pathname === `/results/${searchTerm}`;
-	const isHomePage = location.pathname === "/";
+const Navbar = () => {
+const { searchTerm, setSearchTerm } = useContext(SearchContext);
+const location = useLocation();
+const isResultsPage = location.pathname === `/results/${searchTerm}`;
+const isHomePage = location.pathname === "/";
 
-	useEffect(() => {
-		const storedSearchTerm = localStorage.getItem('searchTerm');
-		if (storedSearchTerm) {
-		setSearchTerm(storedSearchTerm);
-		}
-	}, [setSearchTerm]);
+//keep query in input field
 
-	useEffect(() => {
-		if (searchTerm) {
-		localStorage.setItem('searchTerm', searchTerm);
-		} else {
-		localStorage.removeItem('searchTerm');
-		}
-	}, [searchTerm]);
-
-	if (!isHomePage && !isResultsPage) {
-		return null;
+useEffect(() => {
+	const storedSearchTerm = localStorage.getItem('searchTerm');
+	if (storedSearchTerm) {
+	setSearchTerm(storedSearchTerm);
 	}
+}, [setSearchTerm]);
 
-	return (
-		<nav>
-		<div className={isResultsPage ? "nav-bar-results" : "nav-bar"}>
-			{isResultsPage ? (
-			<>
-				<div className='nav-left'>
-				<Link to={"/"}><img src={Logo} alt="logo" className='logo-result' /></Link>
-				<Search />
-				</div>
+useEffect(() => {
+	if (searchTerm) {
+	localStorage.setItem('searchTerm', searchTerm);
+	} else {
+	localStorage.removeItem('searchTerm');
+	}
+}, [searchTerm]);
 
-				<div className='nav-right-result'>
-				<i className="fas fa-gear nav-icon nav-icon-bg"></i>
-				<button className='nav-icon-bg'>
-					<i className="gg-menu-grid-o nav-icon"></i>
-				</button>
-				<button className="nav-btn">Acceder</button>
-				</div>
-			</>
-			) : (
-			<>
-				<div className='nav-right'>
-				<a href="#" className="nav-link">Gmail</a>
-				<a href="#" className="nav-link">Imágenes</a>
-				<button className='nav-icon-bg'>
-					<i className="gg-menu-grid-o nav-icon"></i>
-				</button>
-				<button className="nav-btn">Acceder</button>
-				</div>
-			</>
-			)}
-		</div>
-		</nav>
-	);
-	};
+if (!isHomePage && !isResultsPage) {
+	return null;
+}
 
-	export default Navbar;
+return (
+	<nav>
+	<div className={isResultsPage ? "nav-bar-results" : "nav-bar"}>
+		{isResultsPage ? (
+		<>
+			<div className='nav-left'>
+			<Link to={"/"}><img src={Logo} alt="logo" className='logo-result' /></Link>
+			<Search />
+			</div>
+
+			<div className='nav-right-result'>
+			<i className="fas fa-gear nav-icon nav-icon-bg"></i>
+			<button className='nav-icon-bg'>
+				<i className="gg-menu-grid-o nav-icon"></i>
+			</button>
+			<button className="nav-btn">Acceder</button>
+			</div>
+		</>
+		) : (
+		<>
+			<div className='nav-right'>
+			<a href="#" className="nav-link">Gmail</a>
+			<a href="#" className="nav-link">Imágenes</a>
+			<button className='nav-icon-bg'>
+				<i className="gg-menu-grid-o nav-icon"></i>
+			</button>
+			<button className="nav-btn">Acceder</button>
+			</div>
+		</>
+		)}
+	</div>
+	</nav>
+);
+};
+
+export default Navbar;
--- frontend-test/src/components/shared/search/Search.tsx
+++ frontend-test/src/components/shared/search/Search.tsx
@@ -4,91 +4,72 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import './Search.css';
 
 const Search = () => {
+const location = useLocation();
+const navigate = useNavigate();
+const { search, searchTerm, setSearchTerm, setErrorMessage } = useContext(SearchContext);
 
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { search, searchTerm, setSearchTerm, setErrorMessage } = useContext(SearchContext);
+const isResultsPage = location.pathname.startsWith(`/results`);
+const [inputValue, setInputValue] = useState(searchTerm || '');
 
-    const isResultsPage = location.pathname.startsWith(`/results`);
-    const [inputValue, setInputValue] = useState(searchTerm || '');
-    const [showErrorMessage, setShowErrorMessage] = useState(false);
+//clean input while in home
 
-    useEffect(() => {
-        if (location.pathname === '/') {
-        setSearchTerm('');
-        setInputValue('');
-        }
-    }, [location.pathname, setSearchTerm]);
+useEffect(() => {
+    if (location.pathname === '/') {
+    setSearchTerm('');
+    setInputValue('');
+    }
+}, [location.pathname, setSearchTerm]);
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-        setInputValue(e.target.value);
-        setShowErrorMessage(false);
-    };
+//handle submit
 
-    const handleInputClean = () => {
-        setInputValue('');
-    };
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!inputValue.trim()) {
-            if(isResultsPage){
-                setErrorMessage("Please provide a valid search term such as: lion, horse, bear");
-            }
-            return;
-        }
-        setErrorMessage(""); 
-        setSearchTerm(inputValue); 
-        navigate(`/results/${inputValue}`);
-        search(inputValue);
-    };
-
-    return (
-        <div className="search-container">
-            <form onSubmit={handleSubmit}>
-
-                <div className={isResultsPage ? 'search-result' : 'search-home'}>
-                    
-                    <button className='search-icon-bg'><i className={isResultsPage ? "icon-result fa fa-search" : "icon-home fa fa-search"}></i></button>
-                    {isResultsPage ? (
-                        <div className='color-icons'>
-                        <i className="fas fa-microphone fa-1x"></i>
-                        <i className="fas fa-camera fa-1x"></i>
-                    </div>
-                    ) : null}
-
-                    {isResultsPage && inputValue ? (
-                        <i onClick={() => handleInputClean()} className="fa fa-times icon-close"></i>
-                    ) : null}
-
-                    <input type="search" value={inputValue} onChange={handleInputChange} placeholder='Buscar' />
-
-                    {!isResultsPage && inputValue ? (
-                        <i onClick={() => handleInputClean()} className="fa fa-times icon-close"></i>
-                    ) : null}
-                    
-                    {!isResultsPage ? (
-                        <div className='color-icons'>  
-                        <i className="fas fa-microphone fa-1x"></i>
-                        <i className="fas fa-camera fa-1x"></i>
-                    </div>
-                    ) : null}
-
-                </div>
-
-                {isResultsPage ? null : (
-                    <button type='submit' className="search-btn">
-                        Buscar
-                    </button>
-                )}
-
-                {showErrorMessage && isResultsPage && (
-                    <p className="error-message">Please enter a search term</p>
-                )}
-
-            </form>
-        </div>
-    );
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputValue.trim()) {
+    if(isResultsPage){
+        setErrorMessage("Please provide a valid search term such as: lion, horse, bear");
+    }
+    return;
+    }
+    setErrorMessage(""); 
+    setSearchTerm(inputValue); 
+    navigate(`/results/${inputValue}`);
+    search(inputValue);
 };
 
-export default Search
+//dynamic icons
+
+const searchIconClass = isResultsPage ? 'icon-result fa fa-search' : 'icon-home fa fa-search';
+const inputCloseIcon = inputValue && <i onClick={() => setInputValue('')} className="fa fa-times icon-close"></i>;
+
+return (
+    <div className="search-container">
+    <form onSubmit={handleSubmit}>
+        <div className={isResultsPage ? 'search-result' : 'search-home'}>
+        <button className='search-icon-bg'><i className={searchIconClass}></i></button>
+        {isResultsPage ? (
+            <div className='color-icons'>
+            {inputCloseIcon}    
+            <i className="fas fa-microphone fa-1x"></i>
+            <i className="fas fa-camera fa-1x"></i>
+            </div>
+        ) : null}
+        <input type="search" value={inputValue} onChange={e => setInputValue(e.target.value)} placeholder='Buscar' />
+        {!isResultsPage && (
+            <div className='color-icons'> 
+            {inputCloseIcon} 
+            <i className="fas fa-microphone fa-1x"></i>
+            <i className="fas fa-camera fa-1x"></i>
+            </div>
+        )}
+        </div>
+        {!isResultsPage && (
+        <button type='submit' className="search-btn">
+            Buscar
+        </button>
+        )}
+    </form>
+    </div>
+);
+};
+
+export default Search;
