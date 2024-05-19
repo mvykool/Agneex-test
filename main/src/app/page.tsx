ddb8bc0d0be9de6fa8e7dc95255264dd4d677f94
mvykool <maykolandres4@gmail.com>
2024-05-19 18:47:58 -0500

    added user auth functionality

--- main/src/app/page.tsx
+++ main/src/app/page.tsx
@@ -1,12 +1,11 @@
-import { db } from "~/server/db"
 import { SignedOut, SignedIn } from "@clerk/nextjs"
+import { getImages } from "~/server/queries"
+
 export const dynamic = "force-dynamic"
 
 async function Images() {
 
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id)
-  });
+  const images = await getImages();
 
   return (
     <div className="flex flex-wrap gap-4">
