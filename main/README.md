719869e92801c26897d8599d423659c1037fe6d2
mvykool <maykolandres4@gmail.com>
2024-05-19 12:30:56 -0500

    removed mock, changed for actual data from db

--- main/README.md
+++ main/README.md
@@ -3,8 +3,8 @@
 - [x] Make it deploy
 - [x] Scaffold basic UI with mock data
 - [x] Tidy up build process
-- [] Actually set up a database (Postgres, w/ Vercel)
-- [] Attach database to UI 
+- [x] Actually set up a database (Postgres, w/ Vercel)
+- [x] Attach database to UI 
 - [] Add authentication
 - [] Add image upload
 - [] Error management (w/ Sentry)
--- main/src/app/page.tsx
+++ main/src/app/page.tsx
@@ -1,28 +1,17 @@
-import Link from "next/link";
-import { db } from "../server/db"
+import { db } from "~/server/db"
 
-const mockUrl = [
-  "https://utfs.io/f/a2cf4bb0-d191-436d-baaf-94be7d96f536-m1u9pv.jpg",
-  "https://utfs.io/f/c89f71a4-9849-45c3-9b79-23bc14ee46e2-26a8m.jpg",
-  "https://utfs.io/f/6544cd21-50b7-448c-948d-9e92977197bb-6uwqs.jpg",
-  "https://utfs.io/f/6f34b978-f770-4814-83b2-1890ea75adcd-rgz8g5.jpeg",
-];
-
-const mockImages = mockUrl.map((url, i) => ({
-  id: i + 1,
-  url,
-}));
+export const dynamic = "force-dynamic"
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts)
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
+        {images.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} alt="image" className="h-36" />
           </div>
